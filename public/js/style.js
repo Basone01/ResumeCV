250d3930da50372d6053016a621a8a1a0e359402
Surachet Sangasaeng <basone01@hotmail.com>
2017-05-10 21:22:16 +0700

    Refix on github failed and Add sticky navbar

---
+++ public/js/style.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  var navtop = $('nav').offset().top;
+  $(window).scroll(function(event) {
+    var view = $(this)[0].scrollY;
+    console.log(view);
+    if(view>navtop-24){
+      $('nav').addClass('nav-fixed');
+    }else{
+      $('nav').removeClass('nav-fixed');
+    }
+  });
+  $("nav").on('click', 'a', function(event) {
+    event.preventDefault();
+    var target = $($(this).attr('href'));
+    $('body').stop(true, false).animate({scrollTop:target.offset().top},{duration:1000},'swing')
+  });
+
+  $(".content-skills").on('click', '.skill-img', function(event) {
+    event.preventDefault();
+    /* Act on the event */
+    var targetData = $(this).children('span').text();;
+    var modal = $(".modal-overlay");
+    var modalHead = modal.children(".modal").children('.modal-head');
+    var modalBody = modalHead.parent().children('.modal-body');
+    modalHead.children('h1').text(targetData);
+    modalBody.text('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+    modal.addClass('active');
+  });
+
+  $("#modal-skill").on('click', function(event) {
+    event.preventDefault();
+    var targetID = $(event.target).attr('id');
+    var modal = $(this)
+    if(targetID=='modal-skill'){
+      modal.removeClass('active');
+    }
+  });
+  $("#modal-skill").on('click', function(event){
+    event.preventDefault();
+    var modal = $('#modal-skill')
+    modal.removeClass('active');
+  });
+});
